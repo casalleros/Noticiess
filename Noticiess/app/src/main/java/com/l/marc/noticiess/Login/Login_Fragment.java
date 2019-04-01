8d5d6ebd8faa1167cbc0040b0edfe6fc0d71f402
casalleros <49169479+casalleros@users.noreply.github.com>
2019-04-01 23:37:28 +0200

    Init

---
+++ Noticiess/app/src/main/java/com/l/marc/noticiess/Login/Login_Fragment.java
@@ -0,0 +1,79 @@
+package com.l.marc.noticiess.Login;
+
+import android.content.Context;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.l.marc.noticiess.NavigationHost;
+import com.l.marc.noticiess.R;
+import com.l.marc.noticiess.Registre.Registre_Fragment;
+
+public class Login_Fragment extends Fragment implements View.OnClickListener {
+
+    private Button btn_autenticar_login;
+    private Button btn_registrar_login;
+
+    private EditText et_correu_login;
+    private EditText et_pass_login;
+
+    private Registre_Fragment Registre_Fragment;
+
+    public Login_Fragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_login_, container, false);
+
+        btn_autenticar_login=v.findViewById(R.id.btn_autenticar_login);
+        btn_registrar_login=v.findViewById(R.id.btn_registrar_login);
+
+        et_correu_login=v.findViewById(R.id.et_correu_login);
+        et_pass_login=v.findViewById(R.id.et_pass_login);
+
+        btn_autenticar_login.setOnClickListener(this);
+        btn_registrar_login.setOnClickListener(this);
+        return v;
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+
+    @Override
+    public void onClick(View v) {
+
+        switch (v.getId()){
+
+            case R.id.btn_autenticar_login:
+                //
+                break;
+            case R.id.btn_registrar_login:
+                registro();
+                break;
+        }
+    }
+
+    private void registro() {
+        Registre_Fragment = new Registre_Fragment();
+        ((NavigationHost) getActivity()).navigateTo(Registre_Fragment,true);//Que fasil!
+    }
+}
---
+++ Noticiess/app/src/main/java/com/l/marc/noticiess/MainActivity.java
@@ -0,0 +1,40 @@
+package com.l.marc.noticiess;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.l.marc.noticiess.Login.Login_Fragment;
+
+public class MainActivity extends AppCompatActivity implements NavigationHost{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.container, new Login_Fragment())
+                    .commit();
+        }
+    }
+
+    @Override
+    public void navigateTo(Fragment fragment, boolean addToBackstack) {//Que fasil!
+
+        FragmentTransaction transaction =
+                getSupportFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.container, fragment);
+
+        if (addToBackstack) {
+            transaction.addToBackStack(null);
+        }
+
+        transaction.commit();
+    }
+
+}
